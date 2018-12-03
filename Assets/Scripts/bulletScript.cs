39eaa4292f76a38d062d418edcc15366f8c5ca4c
rafiqahsns <rafiqhsns@gmail.com>
2018-12-03 14:31:55 +0700

    udah bisa nembak

---
+++ Assets/Scripts/bulletScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletScript : MonoBehaviour {
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		Destroy(this.gameObject);
+		print("hi");
+		if(collision.gameObject.CompareTag("tower"))
+		{
+			Destroy(collision.gameObject);
+			print("hi");
+		}
+	}
+}
