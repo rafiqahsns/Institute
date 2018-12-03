a45c35309c0d6bdba7bbaeba5f7570b24eec4ff6
rafiqahsns <rafiqhsns@gmail.com>
2018-12-03 11:08:55 +0700

    coba

---
+++ Assets/bulletScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletScript : MonoBehaviour {
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		Destroy(this.gameObject);
+		if(collision.gameObject.CompareTag("tower"))
+		{
+			Destroy(collision.gameObject);
+		}
+	}
+}
